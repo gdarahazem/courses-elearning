3d3cdb3a1c6f029872f3596c2a8c5eab7afb63b0
hazemgdara <hazemgdaraa@gmail.com>
2024-08-24 14:16:16 +0100

    fix logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,16 +3,22 @@
         <div class="row align-items-center">
             <div class="col-lg-12">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand logo_1" href="{{ route('home') }}"> <img src="{{ asset('img/single_page_logo.png') }}" alt="logo"> </a>
-                    <a class="navbar-brand logo_2" href="{{ route('home') }}"> <img src="{{ asset('img/logo.png') }}" alt="logo"> </a>
+                    <a class="navbar-brand logo_1" href="{{ route('home') }}"> <img
+                            src="{{ asset('img/single_page_logo.png') }}" alt="logo">
+                        <span class="navbar-text text-white">e-learning</span> <!-- Add this line -->
+                    </a>
+                    <a class="navbar-brand logo_2" href="{{ route('home') }}"> <img
+                            src="{{ asset('img/single_page_logo.png') }}" alt="logo">
+                        <span class="navbar-text">e-learning</span> <!-- Add this line -->
+                    </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div class="collapse navbar-collapse main-menu-item justify-content-end"
-                        id="navbarSupportedContent">
+                         id="navbarSupportedContent">
                         <ul class="navbar-nav align-items-center">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('home') }}">Home</a>
@@ -22,24 +28,28 @@
                             </li>
                             @if($menuDisciplines->count())
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         Disciplines
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         @foreach($menuDisciplines as $id=>$discipline)
-                                            <a class="dropdown-item" href="{{ route('courses.index') }}?discipline={{ $id }}">{{ $discipline }}</a>
+                                            <a class="dropdown-item"
+                                               href="{{ route('courses.index') }}?discipline={{ $id }}">{{ $discipline }}</a>
                                         @endforeach
                                     </div>
                                 </li>
                             @endif
                             @if($menuInstitutions->count())
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         Institutions
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         @foreach($menuInstitutions as $id=>$institution)
-                                            <a class="dropdown-item" href="{{ route('courses.index') }}?institution={{ $id }}">{{ $institution }}</a>
+                                            <a class="dropdown-item"
+                                               href="{{ route('courses.index') }}?institution={{ $id }}">{{ $institution }}</a>
                                         @endforeach
                                     </div>
                                 </li>
@@ -49,12 +59,13 @@
                                     <a class="nav-link" href="{{ route('enroll.myCourses') }}">My Courses</a>
                                 </li>
                                 <li class="d-none d-lg-block">
-                                    <a class="btn_1" href="#" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">Logout</a>
+                                    <a class="btn_1" href="#"
+                                       onclick="event.preventDefault(); document.getElementById('logoutform').submit();">Logout</a>
                                     <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </li>
-                                
+
                             @endauth
                         </ul>
                     </div>
